522b08268970335af9114e71c6ab5b25401aaff3
Mike-18awq <137745753+Mike-18awq@users.noreply.github.com>
2024-03-01 23:51:02 -0500

    Add files via upload

--- config/wal/templates/NvchadWal.lua
+++ config/wal/templates/NvchadWal.lua
@@ -1,5 +1,4 @@
--- Credits to original theme https://github.com/dracula/vim
--- This is a modified version 
+-- Credits to https://github.com/Mike-18awq/dotfiles/blob/main/config/wal/templates/NvchadWal.lua
 
 local M = {{}}
 
@@ -15,13 +14,13 @@ M.base_30 = {{
   grey_fg = "{color8}",
   grey_fg2 = "{color7}",
   light_grey = "{color0}",
-  red = "{color1}",
+  red = "{color1}",--color principal
   baby_pink = "{color13}",
   pink = "{color5}",
   line = "{color8}", -- for lines like vertsplit
-  green = "{color2}",
+  green = "{color1}",--color principal
   vibrant_green = "{color10}",
-  nord_blue = "{color4}",
+  nord_blue = "{color1}",
   blue = "{color12}",
   yellow = "{color3}",
   sun = "{color11}",
@@ -39,18 +38,18 @@ M.base_30 = {{
 M.base_16 = {{
   base00 = "{color0}",
   base01 = "{color8}",
-  base02 = "{color1}",
-  base03 = "{color9}",
+  base02 = "{color1}",--coor principal
+  base03 = "{color9}",--color principal
   base04 = "{color7}",
   base05 = "{color15}",
   base06 = "{color3}",
   base07 = "{color11}",
-  base08 = "{color1}",
-  base09 = "{color9}",
+  base08 = "{color1}",--color principal
+  base09 = "{color9}",--color principal
   base0A = "{color5}",
   base0B = "{color13}",
-  base0C = "{color1}",
-  base0D = "{color9}",
+  base0C = "{color3}",
+  base0D = "{color9}",--color principal
   base0E = "{color4}",
   base0F = "{color15}", -- [] and ,
 }}
