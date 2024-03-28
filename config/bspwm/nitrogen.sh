aeaf0171f726f61fa2c99609f8df749ffd0799d5
Mike-18awq <137745753+Mike-18awq@users.noreply.github.com>
2024-03-28 13:14:11 -0500

    nitrogen.sh

---
+++ config/bspwm/nitrogen.sh
@@ -0,0 +1,9 @@
+# Wallpaper with pywal
+#rm -f ~/.config/nitrogen/bg-saved.cfg
+#ln -s ~/.cache/wal/bg-saved.cfg ~/.config/nitrogen/bg-saved.cfg
+#cd ~/.config/bspwm/ && inkscape --export-filename=Rick2.png --export-area-drawing ~/.cache/wal/Rick2-pywal.svg
+#sleep 1
+#nitrogen --restore
+
+# Wallpaper pywal restore
+#nitrogen --set-scaled $(cat ~/.cache/wal/wal)
