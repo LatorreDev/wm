582b75a16ca85afc6cd0b5ad067e7793023ae005
Mike-18awq <137745753+Mike-18awq@users.noreply.github.com>
2024-03-29 16:12:38 -0500

    wallpaper.sh

--- config/bspwm/wallpaper.sh
+++ config/bspwm/wallpaper.sh
@@ -1,6 +1,1 @@
-cd /home/mike/Imágenes && nitrogen --random --set-scaled Wallpapers/Catppuccin
-cd /home/mike/Imágenes && nitrogen --random --set-scaled Wallpapers/Dracula
-cd /home/mike/Imágenes && nitrogen --random --set-scaled Wallpapers/Catppuccin
-cd /home/mike/Imágenes && nitrogen --random --set-scaled Wallpapers/Nord
-cd /home/mike/Imágenes && nitrogen --random --set-scaled Wallpapers/Dracula
-cd /home/mike/Imágenes && nitrogen --random --set-scaled Wallpapers/Nord
+
