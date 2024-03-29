d9f062809e188148eeaf63c202edae45f37cbbf6
Mike-18awq <137745753+Mike-18awq@users.noreply.github.com>
2024-03-29 16:12:57 -0500

    defaultPrograms.sh

--- config/bspwm/defaultPrograms.sh
+++ config/bspwm/defaultPrograms.sh
@@ -1,2 +1,2 @@
 gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/alacritty
- gsettings set org.cinnamon.desktop.default-applications.terminal exec /usr/bin/alacritty
+gsettings set org.cinnamon.desktop.default-applications.terminal exec /usr/bin/alacritty
